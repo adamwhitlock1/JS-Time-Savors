4ce71895039325b15dbf0ac9635d1850d64c55fd
Adam Whitlock <moose62712@gmail.com>
2018-06-10 12:00:05 -0600

    responsive body class util added

---
+++ js/responsive-body-class.js
@@ -0,0 +1,22 @@
+var body = document.body;
+
+var onresize = function(e) {
+  //note i need to pass the event as an argument to the function
+  var viewportWidth = e.target.outerWidth;
+
+  if (viewportWidth > 768) {
+    body.classList.remove("mobile", "tablet");
+    body.classList.add("desktop");
+  }
+
+  if (viewportWidth <= 768 && viewportWidth >  480) {
+    $("body").removeClass("mobile desktop").addClass("tablet");
+  }
+
+  if (viewportWidth <= 480) {
+    $("body").removeClass("tablet desktop").addClass("mobile");
+  }
+};
+
+checkDevice();
+window.addEventListener("resize", onresize);
